73b823a7664d72cfbd802e87ee624614ab1d682e
Bruno Cordeiro da Silva <bcordeiro.dev@gmail.com>
2025-08-22 12:05:53 -0300

    test: ci/cd

--- config/app.php
+++ config/app.php
@@ -58,7 +58,6 @@ return [
     |--------------------------------------------------------------------------
     | Application Timezone
     |--------------------------------------------------------------------------
-    |
     | Here you may specify the default timezone for your application, which
     | will be used by the PHP date and date-time functions. The timezone
     | is set to "UTC" by default as it is suitable for most use cases.
@@ -75,7 +74,6 @@ return [
     | The application locale determines the default locale that will be used
     | by Laravel's translation / localization methods. This option can be
     | set to any locale for which you plan to have translation strings.
-    |
     */
 
     'locale' => env('APP_LOCALE', 'en'),
