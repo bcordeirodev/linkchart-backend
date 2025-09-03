80a1e35eb2078ac45819173e04b5eb3dabee1e4a
Bruno Cordeiro da Silva <bcordeiro.dev@gmail.com>
2025-09-03 09:45:29 -0300

    sort docker files

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://134.209.33.182:3000')),
+    'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['*'],
 
     'allowed_headers' => ['*'],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
